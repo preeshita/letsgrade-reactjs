c66966a1173534e9cb0b5ee2c09bbd8d72187c24
Preeshita Dey <85007829+preeshita@users.noreply.github.com>
2021-07-14 20:02:13 +0530

    files uploaded
    since the size of the node modules folder is too big, git was not accepting to upload it. hence uploading without node modules folder.

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react"
+import "./styles.css"
+import Button from "./Button"
+const Apple=()=>{
+  return(
+    <div>
+    
+      <h1 style={{textAlign: "center"}}> Hello people </h1><br></br>
+      <h2 style={{textAlign: "center"}}>This is day 1 of learning React js</h2><br></br>
+      
+      <h3>Name of fruits</h3>
+      <Button buttonText="apple"/>
+      <Button buttonText="orange"/>
+      <Button buttonText="kiwi"/>
+      <Button buttonText="banana"/>
+      <Button buttonText="mango"/>
+      <Button buttonText="strawberry"/>
+      <h3>name of sweets</h3>
+      <Button buttonText="donut"/>
+      <Button buttonText="macarons"/>
+      <Button buttonText="brownies"/>
+      <Button buttonText="pastry"/>
+      <Button buttonText="cupcake"/>
+      <Button buttonText="pie"/>
+      <h3>Name of vegetable</h3>
+      <Button buttonText="potato"/>
+      <Button buttonText="tomato"/>
+      <Button buttonText="cabbage"/>
+      <Button buttonText="carrot"/>
+      <Button buttonText="brinjal"/>
+      <Button buttonText="gourd"/>
+    
+      
+      
+       
+      
+        
+      
+     
+     
+      
+    </div>
+  )
+}
+
+export default Apple
